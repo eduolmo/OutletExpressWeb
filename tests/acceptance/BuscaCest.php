56816fa9110721f9a85feb8bab55f0e3734dba71
Paulo Cezar Rocha Furtado <92343021+houselan@users.noreply.github.com>
2023-06-02 08:17:50 -0300

    Update BuscaCest.php

--- tests/acceptance/BuscaCest.php
+++ tests/acceptance/BuscaCest.php
@@ -14,9 +14,12 @@ class BuscaCest
     // tests
     public function buscarResultadosNaPaginaTest(AcceptanceTester $I)
     {
-		$I->amOnPage('/');
-		$I->fillField('searchword', 'edital');
-		$I->click('Buscar');
-		$I->see('resultados encontdos');
+		$I = new AcceptanceTester($scenario);
+		$I->amOnPage('/outlet-web/html/index.html');
+		$I->click('MAIS VENDIDOS');
+		$I->seeCurrentURLEquals('/outlet-web/html/produtos.html');
+		$I->click('Carrinho de Bebê Premium Preto Importado');
+		$I->seeCurrentURLEquals('/outlet-web/html/carrinhoBebe.html');
+		$I->see('Carrinho de Bebê Premium Preto Importado');
     }
 }
