1e25c7ed2f9057284eb71fb49507021254e9260f
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-09-05 09:02:10 -0300

    Add files via upload

--- php/produtos.php
+++ php/produtos.php
@@ -19,13 +19,14 @@
     <section class="secaoPrincipal">
       <!-- div para os produtos-->
             <!--Filtro--> 
-        <button class="btn col-2 d-lg-none botao_filtro" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
+        <button class="btn d-lg-none botao_filtro" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-filter-square" viewBox="0 0 16 16">
                 <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                 <path d="M6 11.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
             </svg>
         </button>
 
+        <!-- inicio do filtro do site -->
         <div class="offcanvas-lg offcanvas-start col-2" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasResponsiveLabel">FILTRO</h5>
@@ -111,7 +112,7 @@
                 </div>
             </div>
         </div>
-                
+        <!-- fim do filtro do site -->     
 
 
         <div class="row produtos col-10">
