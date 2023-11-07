8095b841c3c5d640ae3ec774702be16c9c12df3f
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-11-07 10:18:07 -0300

    Add files via upload

--- php/movel/conexao_db.php
+++ php/movel/conexao_db.php
@@ -2,11 +2,11 @@
 
 // Abre uma conexao com o BD.
 
-$host        = "host = 	suleiman.db.elephantsql.com;";
+$host        = "host = isabelle.db.elephantsql.com;";
 $port        = "port = 5432;";
-$dbname      = "dbname = 	tfhffauf;";
-$dbuser 	 = "tfhffauf";
-$dbpassword	 = "TJAO8aWRj7FvQnWOVIYiuJ8x1LwGQtOd";
+$dbname      = "dbname = aarpgxeh;";
+$dbuser 	 = "aarpgxeh";
+$dbpassword	 = "J6JetsRuN1TROLwMprZK7t7Vq_yQxJzL";
 
 // para conectar ao mysql, substitua pgsql por mysql
 $db_con= new PDO('pgsql:' . $host . $port . $dbname, $dbuser, $dbpassword);
