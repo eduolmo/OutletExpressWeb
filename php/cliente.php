fbe4f93517b2c01c9b7a2929eab91dd44a0011ab
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-11-18 16:00:06 -0300

    Add files via upload

--- php/cliente.php
+++ php/cliente.php
@@ -1,20 +1,54 @@
 <?php
+require_once 'crud.php'
 
-trait Cliente {
-	
+class Cliente extends CRUD{
+
+	protected $table = 'cliente';
 	private $cpf;
+	private $email;
+	private $nome;
+	private $senha;
 		
 		
 	/********Início dos métodos sets e gets*********/
-	public function setcpf($cpf){
+	public function setCpf($cpf){
 		$this->cpf = $cpf;
 	}
-	public function getcpf(){
+	public function getCpf(){
 		return $this->cpf;
 	}
 
 	/********Fim dos métodos sets e gets*********/
 	
+
+	/***************
+	Objetivo: Método que insere um cliente
+	Parâmetro de saída: Retorna true em caso de sucesso ou false em caso de falha.
+	***************/
+	public function insert(){
+		$sql="INSERT INTO usuario/*$this->table*/ (nome,email,senha) VALUES (:nome,:email,:senha)";
+		$stmt = Database::prepare($sql);
+		$stmt->bindParam(':nome', $this->nome);
+		$stmt->bindParam(':email', $this->email);
+		$stmt->bindParam(':senha', $this->senha);
+		
+		return $stmt->execute();
+		
+	}
+	
+	/***************
+	Objetivo: Atuliza um cliente pelo email
+	Parâmetro de entrada: $email - email do cliente
+	Parâmetro de saída: Retorna true em caso de sucesso ou false em caso de falha.
+	***************/
+	public function update($email){
+		$sql="UPDATE $this->table SET nome = :nome, senha = :senha , WHERE email = :email ";
+		$stmt = Database::prepare($sql);
+		$stmt->bindParam(':nome', $this->nome);
+		$stmt->bindParam(':senha', $this->senha);
+		$stmt->bindParam(':email', $email);
+		return $stmt->execute();
+	}
 }
 
 ?>
