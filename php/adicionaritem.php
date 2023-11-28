1df465aadf23fe55c0c07de86df811b74bda36b3
Paulo Cezar Rocha Furtado <92343021+houselan@users.noreply.github.com>
2023-11-28 11:07:44 -0300

    Add files via upload

--- php/adicionaritem.php
+++ php/adicionaritem.php
@@ -1,5 +1,6 @@
 <?php
 // Código PHP para lidar com a inserção/atualização do item_carrinho
+error_reporting(0);
 session_start();
 // Inclui o arquivo da sua classe
 require_once 'item_carrinho.php';
@@ -10,27 +11,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     include 'banco_conexao.php';
     
-    /*
     $db = Database::getInstance();
-
-    $sql = "SELECT email FROM USUARIO WHERE email='$email'";
-    $consulta_usuario_existe = Database::prepare($sql);
-    $consulta_usuario_existe->execute();
-    */
     
-    $itemCarrinho = new item_carrinho();
+    // Obtem o codigo do cliente da sessao
+    $codigo_cliente = $_SESSION['cliente']['codigo'];
+    $codigo_produto = $POST['codigo_produto'];
+    $sql = "
+    SELECT p.codigo as produto_codigo
+    FROM item_carrinho ic
+    JOIN produto p ON ic.fk_PRODUTO_codigo = p.codigo
+    JOIN cliente c ON ic.fk_CLIENTE_FK_USUARIO_codigo = c.fk_USUARIO_codigo
+    WHERE c.fk_USUARIO_codigo = :codigo_cliente
+    ";
 
+    $listacarrinho = $db->prepare($sql);
+    $listacarrinho->bindParam(':codigo_cliente', $codigo_cliente, PDO::PARAM_INT);
+    $listacarrinho->execute();
+    
+    $valor = $listacarrinho->fetch(PDO::FETCH_ASSOC);
+
+
+    $itemCarrinho = new item_carrinho();
+    
 
     // Atribui a quantidade ao objeto
     $itemCarrinho->setQuantidade($_POST['quantidade']);
-    //$itemCarrinho->setFk_CLIENTE_FK_USUARIO_codigo($codigo_cliente);
-    //$itemCarrinho->setFk_PRODUTO_codigo($codigo_produto);
+    $itemCarrinho->setFk_CLIENTE_FK_USUARIO_codigo($codigo_cliente);
+    /itemCarrinho->setFk_PRODUTO_codigo($codigo_produto);
 
 
     // Verifique se o produto já está no carrinho (baseado em alguma chave única)
     // Substitua 'sua_chave_unica' pelo campo correto que você deseja verificar na tabela item_carrinho
     
-    $produtoJaNoCarrinho = $itemCarrinho->verificarProdutoNoCarrinho($_POST['codigo_produto']);
+    $produtoJaNoCarrinho = $itemCarrinho->verificarProdutoNoCarrinho($codigo_produto);
 
     if ($produtoJaNoCarrinho) {
         // Se o produto já está no carrinho, atualize a quantidade
