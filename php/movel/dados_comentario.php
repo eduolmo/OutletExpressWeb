b0575bc62611e7fa372f16fc68892b86557bbe92
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-11-30 18:00:41 -0300

    Add files via upload

--- php/movel/dados_comentario.php
+++ php/movel/dados_comentario.php
@@ -9,70 +9,61 @@
 // conexão com bd
 require_once('conexao_db.php');
 
-// autenticação
-require_once('autenticacao.php');
 
 // array de resposta
 $resposta = array();
 
-// verifica se o usuário conseguiu autenticar
-if(autenticar($db_con)) {
  
-	// Verifica se o parametro id foi enviado na requisicao
-	if (isset($_GET["codigo"])) {
-		
-		// Aqui sao obtidos os parametros
-		$codigo = $_GET['codigo'];
-	 
-		// Obtem do BD os detalhes do produto com id especificado na requisicao GET
-		$consulta = $db_con->prepare("SELECT * FROM COMENTARIO WHERE codigo = $codigo");
-	 
-		if ($consulta->execute()) {
-			if ($consulta->rowCount() > 0) {
-	 
-				// Se o produto existe, os dados completos do produto 
-				// sao adicionados no array de resposta. A imagem nao 
-				// e entregue agora pois ha um php exclusivo para obter 
-				// a imagem do produto.
-				$linha = $consulta->fetch(PDO::FETCH_ASSOC);
-	 
-				$resposta["FK_USUARIO_codigo"] = $linha["FK_USUARIO_codigo"];
-                $resposta["conteudo"] = $linha["conteudo"];
-				$resposta["avaliacao"] = $linha["avaliacao"];		
-				
-				// Caso o produto exista no BD, o cliente 
-				// recebe a chave "sucesso" com valor 1.
-				$resposta["sucesso"] = 1;
-				
-			} else {
-				// Caso o produto nao exista no BD, o cliente 
-				// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
-				// motivo da falha.
-				$resposta["sucesso"] = 0;
-				$resposta["erro"] = "Comentário não encontrado
-                ";
-			}
+// Verifica se o parametro id foi enviado na requisicao
+if (isset($_GET["codigo"])) {
+	
+	// Aqui sao obtidos os parametros
+	$codigo = $_GET['codigo'];
+	
+	// Obtem do BD os detalhes do produto com id especificado na requisicao GET
+	$consulta = $db_con->prepare('SELECT comentario.*,cliente.nome FROM COMENTARIO INNER JOIN CLIENTE ON(cliente.fk_USUARIO_codigo = comentario.fk_cliente_fk_USUARIO_codigo) WHERE comentarios.fk_PRODUTO_codigo = ' . $codigo);
+	
+	if ($consulta->execute()) {
+		if ($consulta->rowCount() > 0) {
+	
+			// Se o produto existe, os dados completos do produto 
+			// sao adicionados no array de resposta. A imagem nao 
+			// e entregue agora pois ha um php exclusivo para obter 
+			// a imagem do produto.
+			$linha = $consulta->fetch(PDO::FETCH_ASSOC);
+	
+			$resposta["nome"] = $linha["nome"];
+			$resposta["comentario"] = $linha["comentario"];
+			$resposta["avaliacao"] = $linha["avaliacao"];		
+			
+			// Caso o produto exista no BD, o cliente 
+			// recebe a chave "sucesso" com valor 1.
+			$resposta["sucesso"] = 1;
+			
 		} else {
-			// Caso ocorra falha no BD, o cliente 
+			// Caso o produto nao exista no BD, o cliente 
 			// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
 			// motivo da falha.
 			$resposta["sucesso"] = 0;
-			$resposta["erro"] = "Erro no BD: " . $consulta->error;
+			$resposta["erro"] = "Comentário não encontrado
+			";
 		}
 	} else {
-		// Se a requisicao foi feita incorretamente, ou seja, os parametros 
-		// nao foram enviados corretamente para o servidor, o cliente 
+		// Caso ocorra falha no BD, o cliente 
 		// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
 		// motivo da falha.
 		$resposta["sucesso"] = 0;
-		$resposta["erro"] = "Campo requerido não preenchido";
+		$resposta["erro"] = "Erro no BD: " . $consulta->error;
 	}
-}
-else {
-	// senha ou usuario nao confere
+} else {
+	// Se a requisicao foi feita incorretamente, ou seja, os parametros 
+	// nao foram enviados corretamente para o servidor, o cliente 
+	// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
+	// motivo da falha.
 	$resposta["sucesso"] = 0;
-	$resposta["error"] = "usuario ou senha não confere";
+	$resposta["erro"] = "Campo requerido não preenchido";
 }
+
 // Fecha a conexao com o BD
 $db_con = null;
 
