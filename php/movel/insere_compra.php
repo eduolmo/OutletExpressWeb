a7162848b885e200dd18b258989b721b5af90cd3
Mariana Lopes de Gouvêa <92120359+Mari14-Lopes@users.noreply.github.com>
2023-12-06 16:48:32 -0300

    Add files via upload

--- php/movel/insere_compra.php
+++ php/movel/insere_compra.php
@@ -1,43 +1,52 @@
 <?php
- 
-/*
- * O seguinte codigo abre uma conexao com o BD e adiciona uma compra nele.
- * As informacoes de um produto sao recebidas atraves de uma requisicao POST.
- */
-
-// conexão com bd
-require_once('conexao_db.php');
-
-// autenticação
-require_once('autenticacao.php');
-
-// array de resposta
-$resposta = array();
-
-// verifica se o usuário conseguiu autenticar
-if(autenticar($db_con)) {
 	
+	/*
+	* O seguinte codigo abre uma conexao com o BD e adiciona uma compra nele.
+	* As informacoes de um produto sao recebidas atraves de uma requisicao POST.
+	*/
+
+	// conexão com bd
+	require_once('conexao_db.php');
+
+	// array de resposta
+	$resposta = array();
+		
 	// Primeiro, verifica-se se todos os parametros foram enviados pelo cliente.
-	if (isset($_POST['forma_pagamento']) && isset($_POST['data'])  && isset($_POST['FK_CLIENTE_codigo'])) {
+	if (isset($_POST['forma_pagamento']) && isset($_POST['email']) && isset($_POST['cpf']) && isset($_POST['cep']) && isset($_POST['rua']) && isset($_POST['numero'])) {
 		
 		// Aqui sao obtidos os parametros
 		$forma_pagamento = $_POST['forma_pagamento'];
-		$data = $_POST['data'];
-        $FK_CLIENTE_codigo = $_POST['FK_CLIENTE_codigo'];		
+		$email = $_POST['email'];
+		$cpf = $_POST['cpf'];
+		$cep = $_POST['cep'];
+		$rua = $_POST['rua'];
+		$numero = $_POST['numero'];
 
-		// A proxima linha insere uma nova compra no BD.
-		// A variavel consulta indica se a insercao foi feita corretamente ou nao.
-		$consulta = $db_con->prepare("INSERT INTO COMPRA(forma_pagamento, data, FK_CLIENTE_codigo) VALUES('$forma_pagamento', '$data', '$FK_CLIENTE_codigo')");
-		if ($consulta->execute()) {
-			// Se o produto foi inserido corretamente no servidor, o cliente 
-			// recebe a chave "sucesso" com valor 1
-			$resposta["sucesso"] = 1;
+		//consulta codigo do cliente pelo email
+		$consulta_cliente = $db_con->prepare("SELECT cliente.fk_usuario_codigo from USUARIO
+		inner join CLIENTE
+		on(cliente.fk_usuario_codigo = usuario.codigo)
+		where(email = '" . $email . "'");
+		$lista_codigo_cliente = $consulta_cliente->fetch(PDO::FETCH_ASSOC);
+		$codigo_cliente = $lista_codigo_cliente["fk_usuario_codigo"];
+
+		//insere uma compra
+		$consulta = $db_con->prepare("INSERT INTO COMPRA(forma_pagamento, FK_CLIENTE_codigo) VALUES('$forma_pagamento', '$data', '$FK_CLIENTE_codigo')");	
+		if ($consulta->execute()) {			
+			//insere endereco do cliente
+			$insere_endereco = $db_con->prepare("INSERT INTO ENDERECO(numero, cep, nome_logradouro) VALUES('$numero', '$cep', '$rua')");
+			$lista_endereco = $consulta_cliente->fetch(PDO::FETCH_ASSOC);
+			$codigo_endereco = $lista_endereco["codigo"];
+			
+			if($insere_endereco->execute()){
+				//insere endereco para cliente
+				$endereco_cliente = $db_con->prepare("INSERT INTO CLIENTE(cpf, fk_usuario_codigo, fk_endereco_codigo) VALUES('$cpf', '$codigo_cliente', '$codigo_endereco')");
+				$resposta["sucesso"] = 1;
+			}
+			
 		} else {
-			// Se o produto nao foi inserido corretamente no servidor, o cliente 
-			// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
-			// motivo da falha.
 			$resposta["sucesso"] = 0;
-			$resposta["erro"] = "Erro ao criar produto no BD: " . $consulta->error;
+			$resposta["erro"] = "Erro ao criar compra no BD: " . $consulta->error;
 		}
 	} else {
 		// Se a requisicao foi feita incorretamente, ou seja, os parametros 
@@ -47,16 +56,11 @@ if(autenticar($db_con)) {
 		$resposta["sucesso"] = 0;
 		$resposta["erro"] = "Campo requerido nao preenchido";
 	}
-}
-else {
-	// senha ou usuario nao confere
-	$resposta["sucesso"] = 0;
-	$resposta["erro"] = "usuario ou senha não confere";
-}
 
-// Fecha a conexao com o BD
-$db_con = null;
 
-// Converte a resposta para o formato JSON.
-echo json_encode($resposta);
+	// Fecha a conexao com o BD
+	$db_con = null;
+
+	// Converte a resposta para o formato JSON.
+	echo json_encode($resposta);
 ?>
