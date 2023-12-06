0ff37b90f000163acfb60d30c9c56e840f50d03f
Eduardo Olmo <91472785+eduolmo@users.noreply.github.com>
2023-12-06 09:52:49 -0300

    Add files via upload

--- php/movel/dados_minhas_compras.php
+++ php/movel/dados_minhas_compras.php
@@ -33,15 +33,14 @@ if(autenticar($db_con)) {
 		$codigo_cliente = $lista_codigo_cliente["fk_usuario_codigo"];
 	 
 		// Obtem do BD os produtos que aquele cliente comprou
-		$consulta = $db_con->prepare("SELECT cliente.fk_usuario_codigo, compra.codigo as "codigo_compra", item_compra.codigo as "codigo_item_compra", produto.codigo as "codigo_produto" from CLIENTE
+		$consulta = $db_con->prepare("SELECT cliente.fk_usuario_codigo, compra.codigo as codigo_compra, item_compra.codigo as codigo_item_compra, produto.codigo as codigo_produto from CLIENTE
 		  inner join COMPRA
-		  on(compra.fk_cliente_fk_usuario_codigo = '" . $codigo_cliente . "')
+		  on(compra.fk_cliente_fk_usuario_codigo = " . $codigo_cliente . ")
 		  inner join ITEM_COMPRA
 		  on(item_compra.fk_compra_codigo = compra.codigo)
 		  inner join PRODUTO
 		  on(item_compra.fk_produto_codigo = produto.codigo)
-		  where cliente.fk_usuario_codigo = '" . $codigo_cliente . "'");		
-	 
+		  where cliente.fk_usuario_codigo = " . $codigo_cliente);		
 		if ($consulta->execute()) {
   		//pega detalhes do produto comprado    			 			
     
