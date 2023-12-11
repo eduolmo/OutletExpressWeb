e03dfcd4a4c54802e198236528c7413fdcf5ba24
Mariana Lopes de Gouvêa <92120359+Mari14-Lopes@users.noreply.github.com>
2023-12-11 11:34:51 -0300

    Add files via upload

--- php/movel/insere_compra.php
+++ php/movel/insere_compra.php
@@ -86,7 +86,7 @@
 			$consulta_insercao->bindParam(':codigo_cliente', $codigo_cliente);
 			$consulta_insercao->bindParam(':data_hora', $data_hora);
 			$consulta_insercao->execute();
-
+			/*
 			$consulta_compra = $db_con->prepare("SELECT * FROM COMPRA WHERE data = :data_hora");
 			$consulta_compra->bindParam(':data_hora', $data_hora);
 			$consulta_compra->execute();
@@ -127,46 +127,16 @@
 				$itemcompra_insercao->bindParam(':qtd', $itens_carrinho[$i]['quantidade']);
 				$itemcompra_insercao->execute();
 			}
-			
+			*/
+
 			$resposta["sucesso"] = 1;
 		}
 		else{
 			$resposta["sucesso"] = 0;
 			$resposta["erro"] = "Erro ao inserir endereco no BD: " . $consulta->error;
 		}
-
-
-
-
-		
-		
-		/*
-		//consulta codigo do cliente pelo email
-		// Consulta código do cliente pelo email
-		$consulta_cliente = $db_con->prepare("SELECT cliente.fk_usuario_codigo FROM USUARIO
-			INNER JOIN CLIENTE ON (cliente.fk_usuario_codigo = usuario.codigo)
-			WHERE email = :email");
-
-		$consulta_cliente->bindParam(':email', $email);
-		$consulta_cliente->execute();
-		$lista_codigo_cliente = $consulta_cliente->fetch(PDO::FETCH_ASSOC);
-		$codigo_cliente = $lista_codigo_cliente["fk_usuario_codigo"];
-
-		//consulta data_hora atual
-		$consulta_data = $db_con->prepare("SELECT CURRENT_TIMESTAMP AT TIME ZONE 'America/Sao_Paulo' AS data_hora_brasil");
-		$consulta_data->execute();
-		$resposta_data = $consulta_data->fetch(PDO::FETCH_ASSOC);
-		$data_hora = $resposta_data["data_hora_brasil"];
-		
-		//insere uma compra
-		$consulta = $db_con->prepare("INSERT INTO COMPRA(forma_pagamento, fk_cliente_fk_usuario_codigo, data) VALUES('$forma_pagamento', $codigo_cliente, '$data_hora')");	
-		*/
 		
 	} else {
-		// Se a requisicao foi feita incorretamente, ou seja, os parametros 
-		// nao foram enviados corretamente para o servidor, o cliente 
-		// recebe a chave "sucesso" com valor 0. A chave "erro" indica o 
-		// motivo da falha.
 		$resposta["sucesso"] = 0;
 		$resposta["erro"] = "Campo requerido nao preenchido";
 	}
